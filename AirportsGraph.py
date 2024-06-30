b8f6fa2a52dc2a5cb6de5efa8b4ac6392b96278d
dashasavochkina <faknig@yandex.ru>
2024-06-30 23:29:04 +0300

    init commit

---
+++ AirportsGraph.py
@@ -0,0 +1,105 @@
+import math
+import string
+
+from pandas import DataFrame
+
+
+def get_travel_time(a_latitude, a_longitude, a_park_time, b_latitude, b_longitude, airplane_speed, airplane_range):
+    dist = get_dist(a_latitude, a_longitude, b_latitude, b_longitude)
+    if dist > airplane_range:
+        return -1
+    return dist / airplane_speed + a_park_time
+
+
+def get_dist(lat1, lon1, lat2, lon2):
+    R = 6371
+    lat1_rad = math.radians(lat1)
+    lon1_rad = math.radians(lon1)
+    lat2_rad = math.radians(lat2)
+    lon2_rad = math.radians(lon2)
+
+    dlat = lat2_rad - lat1_rad
+    dlon = lon2_rad - lon1_rad
+
+    a = math.sin(dlat / 2) ** 2 + math.cos(lat1_rad) * math.cos(lat2_rad) * math.sin(dlon / 2) ** 2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
+    distance = R * c
+
+    return distance
+
+
+class AirportsGraph:
+
+    def __init__(self, airports_df: DataFrame, plane: dict):
+        self.airports_df = airports_df
+        plane_speed = plane['speed']
+        plane_range = plane['range']
+        self.graph = []
+        for i in range(len(airports_df)):
+            a_latitude = airports_df['latitude'].values[i]
+            a_longitude = airports_df['longitude'].values[i]
+            a_park_time = airports_df['park_time'].values[i]
+            dists = []
+            for j in range(len(airports_df)):
+                if i == j:
+                    dists.append(-1)
+                    continue
+                b_latitude = airports_df['latitude'].values[j]
+                b_longitude = airports_df['longitude'].values[j]
+                dists.append(get_travel_time(
+                    a_latitude,
+                    a_longitude,
+                    a_park_time,
+                    b_latitude,
+                    b_longitude,
+                    plane_speed,
+                    plane_range
+                ))
+
+            self.graph.append(dists)
+
+        self.airports_name = dict()
+        for i in range(len(airports_df)):
+            self.airports_name[airports_df['name'].values[i]] = i
+
+    def find_path(self, a_airport_name: string, b_airport_name: string):
+        a_airport_id = self.airports_name[a_airport_name]
+        b_airport_id = self.airports_name[b_airport_name]
+        airports_path = [None for i in range(len(self.graph))]
+        airports_path[a_airport_id] = [0]
+        airports_dist = [math.inf] * len(self.graph)
+        airports_dist[a_airport_id] = 0
+        is_airports_done = [False] * len(self.graph)
+        is_airports_done[a_airport_id] = True
+        next_airport = a_airport_id
+
+        while min(is_airports_done) is False:
+            for i in range(len(airports_dist)):
+                if (
+                    is_airports_done[i] is False
+                    and self.graph[next_airport][i] != -1
+                    and airports_dist[i] > self.graph[next_airport][i] + airports_dist[next_airport]
+                ):
+                    airports_dist[i] = self.graph[next_airport][i] + airports_dist[next_airport]
+                    airports_path[i] = []
+                    for x in airports_path[next_airport]:
+                        airports_path[i].append(x)
+                    airports_path[i].append(i)
+
+            min_id = is_airports_done.index(False)
+            min_val = airports_dist[min_id]
+            for i in range(len(airports_dist)):
+                if is_airports_done[i] is False and airports_dist[i] < min_val:
+                    min_val = airports_dist[i]
+                    min_id = i
+            if min_val == math.inf:
+                break
+            next_airport = min_id
+            is_airports_done[next_airport] = True
+
+        return {
+            'travel_time': airports_dist[b_airport_id] - self.airports_df['park_time'].values[a_airport_id],
+            'travel_route': airports_path[b_airport_id],
+            'travel_finish_time': airports_dist[b_airport_id] - self.airports_df['park_time'].values[a_airport_id]
+        }
+
---
+++ graph_build.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from pandas import DataFrame
+from AirportsGraph import AirportsGraph, get_dist
+
+# 1
+#построение графа аэропортов
+# - считать csv файл с аэропортами
+airports = pd.read_csv('data/airports.csv', sep=';')
+
+planes = pd.read_csv('data/planes.csv', sep=';')
+
+#airports_graph = AirportsGraph(airports, dict(planes['airbus']))
+planes1 = AirportsGraph(airports, planes[planes['name'] == 'airbus 610'].to_dict(orient='records')[0])
+
+print(planes1.find_path('AIR0', 'AIR2'))
+
